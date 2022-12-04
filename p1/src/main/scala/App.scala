656c42adb5fabfa241f24c484b174bff1bf256a9
JKleinCalPoly <78000357+JKleinCalPoly@users.noreply.github.com>
2022-12-03 16:05:15 -0800

    Classifier

--- p1/src/main/scala/App.scala
+++ p1/src/main/scala/App.scala
@@ -1,12 +1,159 @@
-import org.apache.spark.SparkContext._
-import scala.io._
-import org.apache.spark.{ SparkConf, SparkContext }
-import org.apache.spark.rdd._
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
-import scala.collection._
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark._
+
+import java.io.{BufferedWriter, File, FileWriter}
+
 
 object App {
-  def main(args: Array[String]) {
-  }
+    def main(args: Array[String]): Unit = {
+        System.setProperty("hadoop.home.dir", "c:/winutils/")
+        Logger.getLogger("org").setLevel(Level.OFF)
+        Logger.getLogger("akka").setLevel(Level.OFF)
+        val conf = new SparkConf().setAppName("AccidentPredictor").setMaster("local[4]")
+        val sc = new SparkContext(conf)
+
+        val accidents = sc.textFile("US_Accidents_Dec21_updated.csv").map(_.split(',')).persist
+        val file = new File("accidents.csv")
+        val bw = new BufferedWriter(new FileWriter(file))
+        //0, 1, 2, 4, 5, 25, 28
+        val filtered = accidents.map(acc => {
+            val aid = acc(0).split("A-")
+            var id = "0"
+            if(aid.length > 1) { id = aid(1) }
+            var time = acc(2)
+            val dt = time.split(" ")
+            if(dt.length > 1) { time = dt(1) }
+            val timeParts = time.split(":")
+            if(timeParts.length == 3) { time = timeParts(0) }
+            var vis = acc(25)
+            if( vis == "") {vis = "0"}
+            var precip = acc(28)
+            if( precip == "") {precip = "0"}
+            (id, acc(1), time, acc(4), acc(5), vis, precip)})
+            .filter{case (aid: String, _, stt: String, _, _, _, _) => aid.toInt > 0 && stt.toInt > 0}
+            .map{ case (aid: String, sev: String, stt: String, stLat: String,
+                stLng: String, vis: String, precip: String) => (sev, (aid, stt, stLat, stLng, vis, precip))}.persist
+
+        val minGroupSize = filtered.map{case (sev: String, (_, _, _, _, _, _)) => (checkDouble(sev), 1)}
+            .reduceByKey((x, y) => x + y).map{
+            case (sev: Double, count: Int) => (count, sev)}.sortByKey().take(1).take(1)(0)._1
+
+        val sample1 = filtered.filter{case (sev: String, (_, _, _, _, _, _)) => checkDouble(sev) == 1.0 }.map{
+            case (sev: String, (aid: String, stt: String, stLat: String, stLng: String, vis: String, precip: String)) =>
+                (aid, sev, stt, stLat, stLng, vis, precip).productIterator.toArray.mkString(",")}
+        sample1.collect.foreach(str => {
+            bw.write(str)
+            bw.write("\n")})
+
+        val sample2 = filtered.filter{case (sev: String, (_, _, _, _, _, _)) => checkDouble(sev) == 2.0 }.map{
+            case (sev: String, (aid: String, stt: String, stLat: String, stLng: String, vis: String, precip: String)) =>
+                (aid, sev, stt, stLat, stLng, vis, precip).productIterator.toArray.mkString(",")}
+        sample2.take(minGroupSize).foreach(str => {
+            bw.write(str)
+            bw.write("\n")})
+
+        val sample3 = filtered.filter{case (sev: String, (_, _, _, _, _, _)) => checkDouble(sev) == 3.0 }.map{
+            case (sev: String, (aid: String, stt: String, stLat: String, stLng: String, vis: String, precip: String)) =>
+                (aid, sev, stt, stLat, stLng, vis, precip).productIterator.toArray.mkString(",")}
+        sample3.take(minGroupSize).foreach(str => {
+            bw.write(str)
+            bw.write("\n")})
+
+        val sample4 = filtered.filter{case (sev: String, (_, _, _, _, _, _)) => checkDouble(sev) == 4.0 }.map{
+            case (sev: String, (aid: String, stt: String, stLat: String, stLng: String, vis: String, precip: String)) =>
+                (aid, sev, stt, stLat, stLng, vis, precip).productIterator.toArray.mkString(",")}
+        sample4.take(minGroupSize).foreach(str => {
+            bw.write(str)
+            bw.write("\n")})
+
+        bw.close()
+
+        val n = 20
+        val set = sc.textFile("accidents.csv").persist
+        val trainingSet = set
+            .sample(withReplacement = false, .8, 1)
+            .map(line => {
+            val tokens = line.split(',')
+            var accidentVector = List[Double]()
+            accidentVector = List(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+            if(tokens.length > 6){
+                accidentVector = List(
+                    checkDouble(tokens(1)),//sev
+                    checkDouble(tokens(2)),//startTime
+                    checkDouble(tokens(3)),//startLat
+                    checkDouble(tokens(4)),//startLang
+                    checkDouble(tokens(5)),//visibility
+                    checkDouble(tokens(6)))//precipitation
+            }
+            accidentVector
+        }).filter(l => l.head > 0.0).map(vector => (vector.head, vector.tail)).persist
+        println(trainingSet.count())
+
+        val testingSet = set.sample(withReplacement = false, .01, 1).map(line => {
+            val tokens = line.split(',')
+            var accidentVector = List[Double]()
+            accidentVector = List(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+            if(tokens.length > 6){
+                accidentVector = List(
+                    checkDouble(tokens(1)),//sev
+                    checkDouble(tokens(0)),//id
+                    checkDouble(tokens(2)),//startTime
+                    checkDouble(tokens(3)),//startLat
+                    checkDouble(tokens(4)),//startLng
+                    checkDouble(tokens(5)),//visibility
+                    checkDouble(tokens(6)))//precipitation
+            }
+            accidentVector
+        }).filter(l => l.head > 0.0).map(vector => (vector.head, vector.tail)).persist
+        testingSet.take(1).foreach(println)
+        println(testingSet.count())
+
+        testingSet.collect.foreach(acc => {
+            val distances = trainingSet.map{case (severity: Double, vector: List[Double]) => (
+                severity,
+                List(getTimeDistance(acc._2(1), vector.head) * 1.0, //change 1.0 multiplier to change weight
+                    Math.abs(acc._2(2) - vector(1)) * 1.0,
+                    Math.abs(acc._2(3) - vector(2)) * 1.0,
+                    Math.abs(acc._2(4) - vector(3)) * 1.0,
+                    Math.abs(acc._2(5) - vector(4)) * 1.0))}.persist
+            val maxTime = distances.map{case (_, distanceVector: List[Double]) => distanceVector.head}.top(1)
+            val maxLat = distances.map{case (_, distanceVector: List[Double]) => distanceVector(1)}.top(1)
+            val maxLng = distances.map{case (_, distanceVector: List[Double]) => distanceVector(2)}.top(1)
+            val maxVis = distances.map{case (_, distanceVector: List[Double]) => distanceVector(3)}.top(1)
+            val maxPrecip = distances.map{case (_, distanceVector: List[Double]) => distanceVector(4)}.top(1)
+            val eucDistances = distances.map{case (severity: Double, distanceVector: List[Double]) => (
+                Math.sqrt(
+                    scala.math.pow(distanceVector.head / maxTime.head, 2) +
+                        scala.math.pow(distanceVector(1) / maxLat.head, 2) +
+                        scala.math.pow(distanceVector(2) / maxLng.head, 2) +
+                        scala.math.pow(distanceVector(3) / maxVis.head, 2) +
+                        scala.math.pow(distanceVector(4) / maxPrecip.head, 2)),
+                severity)}
+            val topNDistSev = eucDistances.sortByKey(ascending = false).take(n).map{
+                case (_, sev: Double) => (sev, 1)}
+            val sev = sc.parallelize(topNDistSev).reduceByKey((x, y) => x + y).map{
+                case (sev: Double, count: Int) => (count, sev)}.sortByKey(ascending = false).take(1)(0)._2
+            println(acc._2.head.toString + " actual: " + acc._1.toString + " model: " + sev)
+        })
+    }
+
+    def checkDouble(DoubleHuh: String): Double = {
+        try {
+            val doub = DoubleHuh.toDouble
+            doub
+        }
+        catch { case e: Exception => 0.0}
+    }
+
+
+    def getTimeDistance(A: Double, B: Double) : Double = {
+        var ret = Math.abs(A - B)
+        if(A >= B + 12.0) {
+            ret = 12.0 - (A - (B + 12.0))
+        }
+        if(B >= A + 12.0) {
+            ret = 12.0 - (B - (A + 12.0))
+        }
+        ret
+    }
 }
